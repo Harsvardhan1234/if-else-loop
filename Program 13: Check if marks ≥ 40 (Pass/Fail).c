2aa21e683cc6afe3a91f20e9e86329aa7019d3fc
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-02 17:52:41 +0530

    Create Fail).c

---
+++ Program 13: Check if marks ≥ 40 (Pass/Fail).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int marks;
+
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 40) {
+        printf("You have passed.\n");
+    } else {
+        printf("You have failed.\n");
+    }
+}
