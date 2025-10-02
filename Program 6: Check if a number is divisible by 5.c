9091f3b91d966b0f562d3da223ea5a447c7a7d14
Harshvardhan Bhartarinath Dond <150506979+Harsvardhan1234@users.noreply.github.com>
2025-10-02 17:45:37 +0530

    Create Program 6: Check if a number is divisible by 5.c

---
+++ Program 6: Check if a number is divisible by 5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 5 == 0) {
+        printf("The number is divisible by 5.\n");
+    } else {
+        printf("The number is not divisible by 5.\n");
+    }
+}
